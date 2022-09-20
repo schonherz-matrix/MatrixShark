28afa6c913ad3963a05be679bf5287c958cbab84
Bence Cs <bence.csokas@gmail.com>
2022-09-20 07:56:15 +0200

    Updated commands and port, as in the latest firmware

--- matrix4_dissect.lua
+++ matrix4_dissect.lua
@@ -4,21 +4,35 @@
 
 -- Command values
 vs_cmds = {
-	[0] = "NOP",
-	[1] = "REBOOT"
-} -- TODO
+	[0] = "Disable Panels",
+	[1] = "Set Panels Whitebalance",
+	[2] = "Set Panel Whitebalance on Side",
+	[3] = "Use Internal Animation",
+	[4] = "Use External Animation",
+	[5] = "Swap Panels",
+	[6] = "Blank Panels",
+	[7] = "Reboot",
+	[8] = "Start FWU",
+	[9] = "Start Art-Net",
+	[10] = "Start sACN",
+	[11] = "Ping",
+	[12] = "Get Status",
+	[13] = "Get MAC",
+	[14] = "Get FW Checksum",
+	[15] = "Get Panel Status"
+}
 
 -- WireShark objects
-local p_mtx4 = Proto("schmatrix4", "SCH Matrix4")
+local p_mtx4 = Proto("schmtx4", "SCH Matrix4")
 
-local f_cmd    = ProtoField.uint16("schmatrix4.cmd"   , "Command"   , base.HEX, vs_cmds)
-local f_magic  = ProtoField.uint16("schmatrix4.magic" , "Magic bytes")
-local f_data   = ProtoField.string("schmatrix4.data"  , "Data"      , base.HEX)
+local f_cmd    = ProtoField.uint16("schmtx4.cmd"   , "Command"   , base.HEX, vs_cmds)
+local f_magic  = ProtoField.uint16("schmtx4.magic" , "Magic bytes")
+local f_data   = ProtoField.string("schmtx4.data"  , "Data"      , base.HEX)
 
 p_mtx4.fields = { f_cmd, f_magic, f_data }
 
 -- Config for UDP port
-p_mtx4.prefs.udp_port=Pref.uint("UDP port", 1500, "UDP port used by Matrix")
+p_mtx4.prefs.udp_port=Pref.uint("UDP port", 50000, "UDP port used by Matrix4")
 
 -- Raw data dissector (used for payload)
 local d_data = Dissector.get("data")
